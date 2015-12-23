22d390396f8380f031eeedda7652e60ffac8a233
bibi21000 <bibi21000@gmail.com>
2015-12-23 21:54:54 +0100

    Auto-commit

--- src/janitoo_manager/__init__.py
+++ src/janitoo_manager/__init__.py
@@ -36,10 +36,9 @@ __license__ = """
 __author__ = 'Sébastien GALLET aka bibi21000'
 __email__ = 'bibi21000@gmail.com'
 __copyright__ = "Copyright © 2013-2014 Sébastien GALLET aka bibi21000"
-from gevent import monkey
-monkey.patch_all()
 
-import logging
-logger = logging.getLogger(__name__)
-
-#~ from . import create_app
+try:                                                         # pragma: no cover
+    __import__('pkg_resources').declare_namespace(__name__)  # pragma: no cover
+except:                                                      # pragma: no cover
+    # bootstrapping                                          # pragma: no cover
+    pass                                                     # pragma: no cover
